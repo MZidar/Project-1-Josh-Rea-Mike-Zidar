c60cc24525952d1aff53d810f7f943f15e3dee00
Michael Zidar <mzidar@gmail.com>
2021-08-05 11:17:56 -0400

    Event Listeners

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let chooseDrink, drink;
 
 
 
@@ -6,8 +5,12 @@ let chooseDrink, drink;
 
 
 document.querySelector("#pick-drink").addEventListener("submit", (e) => {
-  
+ 
+ 
+
   e.preventDefault()
+
+
   
   chooseDrink = document.querySelector("#pick-drink-input")
   
@@ -21,12 +24,171 @@ document.querySelector("#pick-drink").addEventListener("submit", (e) => {
     pic.src = drink.drinks[0].strDrinkThumb
     
     document.querySelector("#instruction").innerText = drink.drinks[0].strInstructions 
-    
-    document.querySelector("#pick-drink").reset()
 
+
+    const drinkInfo = [];
+    drinkInfo.push(drink.drinks[0])    
+    
+
+    ingArray = [];
+    meaArray = [];
+    document.querySelector("#mea").innerHTML = ""
+    document.querySelector("#ing").innerHTML = ""
+
+    
+
+
+
+
+    for (const [key, value] of Object.entries(drinkInfo[0])) {
+        if (key.includes('strIng'))
+        ingArray.push(value)
+      }
+
+      filterIng = ingArray.filter(function (el) {
+        return el != null;
+      });    
+
+
+
+    for (const [key, value] of Object.entries(drinkInfo[0])) {
+        if (key.includes('strMea'))
+        meaArray.push(value)
+      }
+
+      filterMea = meaArray.filter(function (el) {
+        return el != null;
+      });   
+
+
+
+
+
+    
+    
+   
+
+    filterIng.forEach(el => 
+        {
+            let ingredient = document.createElement("li")
+
+            ingredient.innerText = el
+
+            document.querySelector("#ing").appendChild(ingredient)  
+        })
+
+    filterMea.forEach(el => 
+        {
+            let ingredient = document.createElement("li")
+
+            ingredient.innerText = el
+
+            document.querySelector("#mea").appendChild(ingredient)  
+        })
+       
+    
+    
+
+    document.querySelector("#pick-drink").reset()
+ 
+
+
+   
   })
 })
 
 
 
+
+
+
+document.querySelector("#drop").addEventListener("click", (e) => {
+ 
+
+  e.preventDefault()
+
+
   
+  let dropDrink = e.target.innerText 
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${dropDrink}`)
+  .then(response => response.json())
+  .then((drink) => {
+    
+    console.log(drink.drinks[0])
+
+    let pic = document.querySelector("#drink-pic")
+    pic.src = drink.drinks[0].strDrinkThumb
+    
+    document.querySelector("#instruction").innerText = drink.drinks[0].strInstructions 
+
+
+    const drinkInfo = [];
+    drinkInfo.push(drink.drinks[0])    
+    
+
+    ingArray = [];
+    meaArray = [];
+    document.querySelector("#mea").innerHTML = ""
+    document.querySelector("#ing").innerHTML = ""
+
+    
+
+
+
+
+    for (const [key, value] of Object.entries(drinkInfo[0])) {
+        if (key.includes('strIng'))
+        ingArray.push(value)
+      }
+
+      filterIng = ingArray.filter(function (el) {
+        return el != null;
+      });    
+
+
+
+    for (const [key, value] of Object.entries(drinkInfo[0])) {
+        if (key.includes('strMea'))
+        meaArray.push(value)
+      }
+
+      filterMea = meaArray.filter(function (el) {
+        return el != null;
+      });   
+
+
+
+
+
+    
+    
+   
+
+    filterIng.forEach(el => 
+        {
+            let ingredient = document.createElement("li")
+
+            ingredient.innerText = el
+
+            document.querySelector("#ing").appendChild(ingredient)  
+        })
+
+    filterMea.forEach(el => 
+        {
+            let ingredient = document.createElement("li")
+
+            ingredient.innerText = el
+
+            document.querySelector("#mea").appendChild(ingredient)  
+        })
+       
+    
+    
+
+    document.querySelector("#pick-drink").reset()
+ 
+
+
+   
+  })
+})
---
+++ scraps.js
@@ -0,0 +1,7 @@
+<li>
+<a class="test" tabindex="-1" href="#"><span class="caret"></span></a>
+<ul class="dropdown-menu4">
+<li><a tabindex="-1" href="#"></a></li>
+
+</ul>
+</li>
